dce9f15412320d6d7c8e2caa46f4994d6c41fc4c
Thorben Johr <thjo00002@stud.uni-saarland.de>
2025-09-15 11:23:05 +0200

    update

--- src/parser/Accumulator.kt
+++ src/parser/Accumulator.kt
@@ -1,4 +1,34 @@
 package parser
 
+import actions.Farm
+import environmental.Cloud
+import incidents.Incident
+import layout.Tile
+
 class Accumulator {
+    private var tiles = mapOf<Int, Tile>()
+    private var farms = mapOf<Int, Farm>()
+    private var clouds = mapOf<Int, Cloud>()
+    private var incidents = mapOf<Int, Incident>()
+
+    fun getTiles() = tiles
+    fun getFarms() = farms
+    fun getClouds() = clouds
+    fun getIncidents() = incidents
+
+    fun setTiles(newTiles: Map<Int, Tile>) {
+        tiles = newTiles
+    }
+
+    fun setFarms(newFarms: Map<Int, Farm>) {
+        farms = newFarms
+    }
+
+    fun setClouds(newClouds: Map<Int, Cloud>) {
+        clouds = newClouds
+    }
+
+    fun setIncidents(newIncidents: Map<Int, Incident>) {
+        incidents = newIncidents
+    }
 }
