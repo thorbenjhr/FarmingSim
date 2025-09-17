5f2fd99a3ae5592e8714b16a6a3cd39ac0cae135
Thorben <thorben.johr@googlemail.com>
2025-09-17 21:47:19 +0200

    rough idea

--- src/environmental/Cloud.kt
+++ src/environmental/Cloud.kt
@@ -1,12 +1,24 @@
 package environmental
 
-data class Cloud(private var id: Int, private val location: Int, private val duration: Int, private val amount: Int) {
+data class Cloud(private var id: Int, private var location: Int, private var duration: Int, private var amount: Int) {
     fun getId() = id
     fun getLocation() = location
     fun getDuration() = duration
     fun getAmount() = amount
 
+    fun setLocation(newLocation: Int) {
+        location = newLocation
+    }
+
     fun setId(newId: Int) {
         id = newId
+    }
+
+    fun setDuration(newDuration: Int) {
+        duration = newDuration
+    }
+
+    fun setAmount(newAmount: Int) {
+        amount = newAmount
     }
 }
--- src/environmental/CloudManager.kt
+++ src/environmental/CloudManager.kt
@@ -1,38 +1,65 @@
 package environmental
 
 import Constants
+import enums.TileType
 import layout.MapClass
+import logging.Logger
+import java.util.PriorityQueue
 import kotlin.math.min
 
 class CloudManager(private var map: MapClass, private var clouds: MutableMap<Int, Cloud>) {
-    private var tileToCloud: MutableMap<Int, Int> = mutableMapOf()
+    private var maxId = clouds.keys.max()
+    private var tileToCloud: MutableMap<Int, Int> = clouds.values.associateBy({ it.getLocation() }, {it.getId()}).toMutableMap()
+    private var newClouds: PriorityQueue<Pair<Int, Cloud>> = PriorityQueue(compareBy { it.second.getId() })
 
-    fun applyMoves() {
-        clouds.keys.forEach { id -> computeNextLocations(id) }
+    fun applyMoves(yearTick: Int) {
+        clouds.keys.forEach{ c -> computeNextLocations(c, Constants.MAX_MOVE_PER_TICK, yearTick) }
+        newClouds.forEach{c -> computeNextLocations(c.second.getId(), c.first, yearTick)}
+        newClouds.clear()
     }
 
-    private fun computeNextLocations(id: Int) {
-        // TODO()
+    private fun computeNextLocations(id: Int, movesLeft: Int, yearTick: Int) {
+        val cloud = clouds[id]
+        val tile = map.getTileByIndex(cloud?.getLocation()!!)
+        applyRain(id)
+
+        for (move in movesLeft downTo 0) {
+            val nextTile = map.getTileByIdAndDirection(cloud.getLocation(), tile?.getDirection()!!)
+            if (nextTile?.getType() == TileType.VILLAGE) {
+                TODO()
+            }
+            // fix rain amount
+            Logger.logCloudMovement(cloud.getId(), 3, cloud.getLocation(), nextTile?.getId()!!)
+            Logger.logCloudMovementSunlight(tile.getId(), Constants.SUNLIGHT_BASE_HOURS_PER_MONTH[yearTick]!! - tile.getEnvironment()?.getSunlightPenalty()!!)
+        }
     }
 
     private fun applyRain(id: Int) {
-        TODO()
+        // TODO()
     }
 
     private fun decrementAndDissipate(id: Int) {
-        TODO()
+        val cloud = clouds[id]
+        cloud?.setDuration(cloud.getDuration() - 1)
+        if (cloud?.getDuration() == 0) {
+            clouds.remove(id)
+            tileToCloud.remove(cloud.getLocation())
+            Logger.logCloudDissipation(id, cloud.getLocation())
+        }
     }
 
-    private fun applyMerge(onTileId: Int, movingId: Int, movesLeft: Int): Pair<Int, Cloud> {
+    private fun applyMerge(onTileId: Int, movingId: Int, movesLeft: Int) {
         val cloudOnTile = clouds[onTileId]
         val cloudMoving = clouds[movingId]
-        val c = Cloud(clouds.keys.max() + 1, cloudOnTile!!.getLocation(), min(cloudOnTile.getDuration(), cloudMoving!!.getDuration()), cloudOnTile.getAmount() + cloudMoving.getAmount())
+        maxId += 1
+        val c = Cloud(maxId, cloudOnTile!!.getLocation(), min(cloudOnTile.getDuration(), cloudMoving!!.getDuration()), cloudOnTile.getAmount() + cloudMoving.getAmount())
         val moves = min(movesLeft, Constants.MAX_MOVE_PER_TICK)
-        return Pair(moves, c)
+        newClouds.add(Pair(moves, c))
     }
 
     fun addCloud(cloud: Cloud) {
-        cloud.setId(clouds.keys.max() + 1)
+        maxId += 1
+        cloud.setId(maxId)
         clouds[cloud.getId()] = cloud
         tileToCloud[cloud.getLocation()] = cloud.getId()
     }
